4c7c4f7b4c2a81e518cc577f080cc3d060f4f688
Vitaly Penzev <vspenzev@eleron.org>
2024-11-27 10:25:30 +0300

    All methods added 2

--- from_the_90x.py
+++ from_the_90x.py
@@ -20,15 +20,43 @@ class Task:
         self.cursor.execute("DELETE FROM tasks WHERE name = ?", (name,))
         self.conn.commit()
 
-    # def get_completed(self):
-    #     self.cursor.execute("SELECT deadline, name, description, priority, status FROM tasks WHERE status = ?", (True,))
-    #
-    #
-    # def get_overdue(self):
-    #     self.cursor.execute('SELECT deadline, name, description, priority, status FROM tasks WHERE status = ?', (None,))
-    # def check(self):
-    #     d = date.today
-    #     self.cursor.execute("SELECT deadline FROM tasks WHERE deadline < ?", (d))
+    def get_completed(self):
+        self.cursor.execute("SELECT deadline, name, description, priority, status FROM tasks WHERE status = ?", (True,))
+        for all_tasks in self.cursor.fetchall():
+            tasks = {'Deadline:  ': all_tasks[0]}
+            tasks['Задача:    '] = all_tasks[1]
+            tasks['Описание:  '] = all_tasks[2]
+            tasks['Приоритет: '] = all_tasks[3]
+            if all_tasks[4] == 1:
+                tasks['Статус:    '] = 'Выполнено'
+            elif all_tasks[4] == 0:
+                tasks['Статус:    '] = 'Не выполнено'
+            else:
+                tasks['Статус:    '] = 'Просрочено'
+            for fields in tasks.items():
+                print(*fields)
+            print()
+
+    def get_overdue(self):
+        self.cursor.execute('SELECT deadline, name, description, priority, status FROM tasks WHERE status IS NULL')
+        for all_tasks in self.cursor.fetchall():
+            tasks = {'Deadline:  ': all_tasks[0]}
+            tasks['Задача:    '] = all_tasks[1]
+            tasks['Описание:  '] = all_tasks[2]
+            tasks['Приоритет: '] = all_tasks[3]
+            if all_tasks[4] == 1:
+                tasks['Статус:    '] = 'Выполнено'
+            elif all_tasks[4] == 0:
+                tasks['Статус:    '] = 'Не выполнено'
+            else:
+                tasks['Статус:    '] = 'Просрочено'
+            for fields in tasks.items():
+                print(*fields)
+            print()
+
+    def check(self, date):
+        self.cursor.execute("UPDATE tasks SET status = NULL WHERE deadline < ? AND status == 0", (str(date),))
+        self.conn.commit()
 
     def update_task(self, new_name, new_description, new_deadline, new_priority, name):
         self.cursor.execute("UPDATE tasks SET name = ?, description = ?, deadline = ?, priority = ? WHERE name = ?", (new_name, new_description, new_deadline, new_priority, name))
@@ -47,12 +75,13 @@ class Task:
             tasks['Приоритет: '] = all_tasks[3]
             if all_tasks[4] == 1:
                 tasks['Статус:    '] = 'Выполнено'
-            else:
+            elif all_tasks[4] == 0:
                 tasks['Статус:    '] = 'Не выполнено'
+            else:
+                tasks['Статус:    '] = 'Просрочено'
             for fields in tasks.items():
                 print(*fields)
             print()
-
 
     def sort_by(self, by):
         self.cursor.execute("SELECT deadline, name, description, priority, status FROM tasks;")
@@ -64,29 +93,32 @@ class Task:
             tasks['Приоритет: '] = all_tasks[3]
             if all_tasks[4] == 1:
                 tasks['Статус:    '] = 'Выполнено'
-            else:
+            elif all_tasks[4] == 0:
                 tasks['Статус:    '] = 'Не выполнено'
+            else:
+                tasks['Статус:    '] = 'Просрочено'
             for fields in tasks.items():
                 print(*fields)
             print()
 
-
-    # def filter_by(self, by):
-    #     self.cursor.execute("""SELECT deadline, name, description, priority, status
-    #                         FROM tasks WHERE deadline == ?;""", (by)
-    #                         )
-    #     for all_tasks in self.cursor.fetchall():
-    #         tasks = {'Deadline:  ': all_tasks[0]}
-    #         tasks['Задача:    '] = all_tasks[1]
-    #         tasks['Описание:  '] = all_tasks[2]
-    #         tasks['Приоритет: '] = all_tasks[3]
-    #         if all_tasks[4] == 1:
-    #             tasks['Статус:    '] = 'Выполнено'
-    #         else:
-    #             tasks['Статус:    '] = 'Не выполнено'
-    #         for fields in tasks.items():
-    #             print(*fields)
-    #         print()
+    def filter_by(self, by):
+        self.cursor.execute("""SELECT deadline, name, description, priority, status
+                            FROM tasks WHERE deadline == ?;""", (by)
+                            )
+        for all_tasks in self.cursor.fetchall():
+            tasks = {'Deadline:  ': all_tasks[0]}
+            tasks['Задача:    '] = all_tasks[1]
+            tasks['Описание:  '] = all_tasks[2]
+            tasks['Приоритет: '] = all_tasks[3]
+            if all_tasks[4] == 1:
+                tasks['Статус:    '] = 'Выполнено'
+            elif all_tasks[4] == 0:
+                tasks['Статус:    '] = 'Не выполнено'
+            else:
+                tasks['Статус:    '] = 'Просрочено'
+            for fields in tasks.items():
+                print(*fields)
+            print()
 
     def find_by(self, by):
         self.cursor.execute("""SELECT deadline, name, description, priority, status 
@@ -111,12 +143,13 @@ class Task:
 
 if __name__ == '__main__':
     create_db()
+    d = date.today()
     task = Task()
+    task.check(d)
     print('Ваши задачи:')
     print()
-    # task.check()
-    task.get_all()
-
+    # task.get_all()
+    task.get_overdue()
     start = 'да'
     while start == 'да':
         print('Желаете добавить новые задачи? ')
